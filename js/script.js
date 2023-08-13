378f1597d975059febc60381415ebf128c6de2a5
kokurate <ericksanger66@gmail.com>
2023-08-13 14:23:44 +0800

    Create Text Typing Animation in Home Section

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ============================ Typing Animation ============================ */
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Learner",
+    "Cyber Security Enthusiast",
+    "Web Developer",
+    "Multimedia Officer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
